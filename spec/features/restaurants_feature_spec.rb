3bd606c1f881b08b074ce09c7ef7657a53960cf5
Natalie Akam <natalie.akam@gmail.com>
2015-07-20 14:14:03 +0100

    Added restaurants controller

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -4,7 +4,7 @@ feature 'restaurants' do
   context 'no restaurants have been added' do
     scenario 'should display a prompt to add a restaurant' do
       visit '/restaurants'
-      expect(page).to have_content 'No restaurant yet'
+      expect(page).to have_content 'No restaurants yet'
       expect(page).to have_link 'Add a restaurant'
     end
   end  
