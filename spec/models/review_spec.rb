229617d41d6d6576201e83fbb6bfc74c82d299e0
Natalie Akam <natalie.akam@gmail.com>
2015-07-21 15:47:38 +0100

    Added rating max/min validation

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
-describe Review do
+describe Review , :type => :model do
   it { should belong_to(:restaurant) }
+
+  it "is invalid if the rating is more than 5" do
+    review = Review.new(rating: 10)
+    expect(review).to have(1).error_on(:rating)
+  end
 end
