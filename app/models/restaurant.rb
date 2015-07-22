6c0364452d296ff04ec753bd475462b3f15fbf16
Zeina Dajani <dajani.zeina@gmail.com>
2015-07-22 18:30:53 +0100

    limited signed user reviews to one

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,12 +1,13 @@
 class Restaurant < ActiveRecord::Base
-  has_many :reviews, dependent: :destroy do
-    def build_review(attributes = {}, user)
-      review = reviews.build(attributes)
-      review.user = user
-      review
-    end
-  end
+  has_many :reviews,
+        -> { extending WithUserAssociationExtension }, 
+        dependent: :restrict_with_exception
   belongs_to :user
   validates :name, length: {minimum: 3}, uniqueness: true
 
+  # def build_review(attributes = {}, user)
+  #   review = reviews.build(attributes)
+  #   review.user = user
+  #   review
+  # end
 end
