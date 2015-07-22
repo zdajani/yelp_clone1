24135e661772d9ed6062fdd65f5043fb8b3206c5
Zeina Dajani <dajani.zeina@gmail.com>
2015-07-22 11:18:36 +0100

    facebook sign in change of place

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,17 +1,17 @@
 class ReviewsController < ApplicationController
 
-def new
-  @restaurant = Restaurant.find(params[:restaurant_id])
-  @review = Review.new
-end
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
 
-def create
-  @restaurant = Restaurant.find(params[:restaurant_id])
-  @restaurant.reviews.create(review_params)
-  redirect_to restaurants_path
-end 
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @restaurant.reviews.create(review_params)
+    redirect_to restaurants_path
+  end 
 
-def review_params
-  params.require(:review).permit(:thoughts, :rating)
-end
+  def review_params
+    params.require(:review).permit(:thoughts, :rating)
+  end
 end
