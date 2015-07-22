0633ce98f46e5d7e0fd37384e8a0a1ea3f5e3f48
Natalie Akam <natalie.akam@gmail.com>
2015-07-22 12:15:42 +0100

    Added test for removing reviews from database when restaurant deleted

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,10 +1,17 @@
-require 'spec_helper'
+require 'rails_helper'
 
-describe Review , :type => :model do
+describe Review , :type => :model do	
   it { should belong_to(:restaurant) }
 
   it "is invalid if the rating is more than 5" do
     review = Review.new(rating: 10)
     expect(review).to have(1).error_on(:rating)
+  end
+
+  it 'is removed from the database when restaurant deleted' do
+    restaurant = Restaurant.create(name: 'subway')
+    restaurant.reviews.create(thoughts: 'ok sub', rating: 3)
+    restaurant.destroy
+    expect(Review.find_by(thoughts: 'ok sub')).to eq nil
   end
 end
